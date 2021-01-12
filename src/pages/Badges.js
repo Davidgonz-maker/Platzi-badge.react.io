87645215941a61ccde20816f2023baeaf62d4e83
David G <davidgonzalez0519@gmail.com>
2021-01-12 15:43:32 -0500

    last changes after formatig pc

---
+++ src/pages/Badges.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Navbar from '../components/Navbar';
+import '../components/styles/'
+
+class Badges extends React.Component{
+  render() {
+    return(
+      <div>
+        <Navbar />
+        <div className="Badges">
+          <div className="Badges__hero">
+            <div></div>
+
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Badges;  
